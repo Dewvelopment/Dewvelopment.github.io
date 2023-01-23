010d8cf8e5fa39e0462510ea5f2374a787821440
Dewvelopment <123356739+Dewvelopment@users.noreply.github.com>
2023-01-23 15:07:56 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>CopyRight By DewwwZZ</title>
+	<link rel="stylesheet" type="text/css" href="style1.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+</head>
+<body>
+	<!-- header -->
+	<div class="medsos">
+		<div class="container">
+			<ul>
+				<li><a href="https://wa.me/628978117925?text=Hallo,DewwzzShop"><i class="fab fa-whatsapp"></i></a></li>
+				<li><a href="https://www.youtube.com/channel/UC0Pcu6RVUi-U6jZvnA59Z2Q?app=desktop"><i class="fab fa-youtube"></i></a></li>
+				<li><a href="https://www.instagram.com/dewwww.28"><i class="fab fa-instagram"></i></a></li>
+			</ul>
+		</div>
+	</div>
+	<header>
+		<div class="container">
+			<h1><a href="home.php">Dewwzz</a></h1>
+			<ul>
+				<li><a href="galery.html">GALERY</a></li>
+				<li><a href="contact.php">CONTACT</a></li>
+				<li><a href="develop.html">TEAM</a></li>
+				<li><a href="video.html">VIDEO</a></li>
+				<li><a href="new1.html">NEW</a></li>
+				<li><a href="index.php"><i class="fa fa-user" style="font-size:18px;color:red"></a></li></i>
+				<li><a href="dewa.php">LOGOUT</a></li>
+			</ul>
+		</div>
+	</header>
+
+	<!-- banner -->
+	<section class="banner">
+		<h2>WELCOME TO MY WEBSITE</h2>
+	</section>
+
+	<!-- about -->
+	<section class="about">
+		<div class="container">
+			<h3>QUOTES</h3>
+			<p>"Yang terpenting bukan seberapa banyak strategi yang kita tahu, tetapi seberapa cepat kita menjalankan strategi tersebut untuk mencapai kesuksesan."
+			<center><br>"Ketika kamu gagal, Debuglah masalahmu dan perbaiki bug itu."</center>
+			<center><br>"Ketika kamu berhasil, sharelah 'Source Codemu' kepada yang membutuhkan agar codingan mu tak sia-sia."</p></center>
+		</div>
+	</section>
+
+	<!-- service -->
+	<section class="service">
+		<div class="container">
+			<h3>SERVICE</h3>
+			<div class="box">
+				<div class="col-4">
+					<div class="icon"><i class="fab fa-instagram"></i></div>
+					<h4>INSTAGRAM</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-globe"></i></div>
+					<h4>WEB DEVELOPMENT</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-edit"></i></div>
+					<h4>DESIGN</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-chart-bar"></i></div>
+					<h4>DIGITAL MARKETING</h4>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- footer -->
+	<footer>
+		<div class="container">
+			<small>Copyright &copy; 2022 - Dewwzz. All Rights Reserved.</small>
+		</div>
+	</footer>
+
+
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$(".bg-loader").hide();
+		})
+	</script>
+</body>
+</html>
